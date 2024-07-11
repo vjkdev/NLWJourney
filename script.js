c011af65a509eccc311c4cb3e0053a0c10ba9091
Vanderlei Jose Kempf <vandercurso@gmail.com>
2024-07-11 09:09:42 -0300

    Criação do estilo e finalização projeto HTML, CSS, JavaScript

--- script.js
+++ script.js
@@ -56,10 +56,29 @@ const criarItemDaAtividade = (atividade) => {
     const formatar = formatador(atividade.data);
 
     return `
-    <div>
+    <div class="card-bg">
         ${input}
-        <span>${atividade.nome}</span>
-        <time>
+
+        <div>
+            <svg class="active" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7.50008 9.99999L9.16675 11.6667L12.5001 8.33332M18.3334 9.99999C18.3334 14.6024 14.6025 18.3333 10.0001 18.3333C5.39771 18.3333 1.66675 14.6024 1.66675 9.99999C1.66675 5.39762 5.39771 1.66666 10.0001 1.66666C14.6025 1.66666 18.3334 5.39762 18.3334 9.99999Z" stroke="#BEF264" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+
+            <svg class="inactive" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8.41664 1.81833C9.46249 1.61593 10.5374 1.61593 11.5833 1.81833M11.5833 18.1817C10.5374 18.3841 9.46249 18.3841 8.41664 18.1817M14.6741 3.10083C15.5587 3.70019 16.3197 4.46406 16.9158 5.35083M1.8183 11.5833C1.6159 10.5375 1.6159 9.46252 1.8183 8.41667M16.8991 14.6742C16.2998 15.5587 15.5359 16.3198 14.6491 16.9158M18.1816 8.41667C18.384 9.46252 18.384 10.5375 18.1816 11.5833M3.1008 5.32583C3.70016 4.44128 4.46403 3.68023 5.3508 3.08417M5.3258 16.8992C4.44124 16.2998 3.6802 15.5359 3.08414 14.6492" stroke="#A1A1AA" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+
+
+            <span>${atividade.nome}</span>
+        </div> 
+
+        <time class="short">
+        ${formatar.dia.semana.curto}.
+        dia ${formatar.dia.numerico} <br>
+        às ${formatar.hora}h
+        </time>
+
+        <time class="full">
         ${formatar.dia.semana.longo}, 
         dia ${formatar.dia.numerico}
         de ${formatar.mes}
